18c711bcdd12af27b22bc3d868e0d17384fa0709
keisuke.teraoka <keisuke.teraoka@asoview.co.jp>
2019-03-11 13:28:27 +0900

    begin implementing recommendations

---
+++ chapter_02/similarity.py
@@ -0,0 +1,13 @@
+#%%
+from math import sqrt
+
+sqrt(pow(5-4, 2) + pow(4-1, 2))
+
+#%%
+1 / (1 + sqrt(pow(5-4, 2) + pow(4-1, 2)))
+
+#%%
+from chapter_02 import recommendations
+recommendations.sim_distance(recommendations.critics, 'Lisa Rose', 'Gene Seymour')
+
+#%%
